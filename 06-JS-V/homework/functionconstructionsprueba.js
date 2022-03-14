bf79f83c01c0f188802469e6c00c9f0424152edc
Keisy <kd.72002@outlook.com>
2022-03-14 15:18:03 -0500

    cambios 2

--- 06-JS-V/homework/functionconstructionsprueba.js
+++ 06-JS-V/homework/functionconstructionsprueba.js
@@ -1,5 +1,5 @@
 function Car (marca, modelo, año, color){
-    this.marca = marca  || "Volkswagen";  //default operator
+    this.marca = marca  || "Volkswagen";  //default operator si es que no le paso un valor entonces se tiene un valor por defecto
     this.modelo = modelo || "3000"; // this no retorna nada aquí
     this.año = año || "1968";
     this.color = color || "azul";
@@ -17,6 +17,39 @@ console.log(audi)
 // Car ('audi', 1500, 'verde').bind(objeto) ; // bind, el this de la funcion CAR tiene que ser objeto. 
 // return objeto;
 
+
+var persona1 = {
+    edad: 23, 
+    getEdad: function(){   //misma función
+        return this.edad
+    }
+}
+
+var persona2 = {
+    edad: 16, 
+    getEdad: function(){
+        return this.edad
+    }
+}
+
+var persona3 = {
+    edad: 7, 
+    getEdad: function(){
+        return this.edad
+    }
+}
+
+function Persona (edad){
+    this.edad = edad;
+}
+
+Persona.prototype.getEdad = function (){
+   return this.edad;
+}
+
+var juan = new Persona (23);
+var juancho = new Persona (16);
+var juanito = new Persona (7);
 
 
 
